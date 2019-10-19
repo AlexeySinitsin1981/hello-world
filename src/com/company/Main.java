4fccededc5107d5a6b6a3d865c6bc7c6a9e093d6
AlexeySinitsin1981 <20035@list.ru>
2019-10-19 10:37:07 +0300

    test2

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Start");
+        String s ="She is a doctor";
+        System.out.println(s.indexOf("is"));
+    }
+}
---
+++ src/com/company/Test1.java
@@ -0,0 +1,14 @@
+package com.company;
+
+
+public class Test1 {
+    public static void main(String[] args) {
+        String a = "Hello";
+        char p;
+        for (int i = a.length()-1; i >=0 ; i--) {
+            p=a.charAt(i);
+            System.out.print(p);
+
+        }
+    }
+}
